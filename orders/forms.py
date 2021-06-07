e65824684d9d59d00c2eae5db82c469d82e9b507
jattoo <m_krub@aol.com>
2021-06-07 20:53:35 +0300

    added ordering functionalities

---
+++ orders/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from django.forms import fields
+from .models import Order
+
+class OrderCreateForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ['first_name', 'last_name', 'email', 'address', 'post_code', 'city']
